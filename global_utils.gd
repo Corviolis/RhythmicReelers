2a872b0c890ccac23abe97e52af60a3576816bb3
Ethan Meier <e652@hotmail.ca>
2024-05-22 20:39:35 -0700

    added singleton scene loading

--- global_utils.gd
+++ global_utils.gd
@@ -1,1 +1,13 @@
 extends Node
+
+var current_scene := get_tree().current_scene
+
+func goto_scene(path: String):
+    call_deferred(&"_deferred_goto_scene", path)
+
+func _deferred_goto_scene(path: String):
+    current_scene.free()
+    var new_scene := ResourceLoader.load(path) as PackedScene
+    current_scene = new_scene.instantiate()
+    get_tree().root.add_child(current_scene)
+    get_tree().current_scene = current_scene
