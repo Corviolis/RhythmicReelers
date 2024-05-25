9ff6de139c2a64888455385dfc43daa0ed38b179
MrNavaStar <Mr.NavaStar@gmail.com>
2024-05-25 01:24:06 -0600

    custom file parsing

--- rhythm_engine.gd
+++ rhythm_engine.gd
@@ -9,45 +9,70 @@ var delay: float
 var song_position: float
 var sessions = []
 
+class Session:
+	var tracks: Dictionary
+	
+	func _init(map: String):
+		tracks = RhythmEngine.beat_maps[map]
+	
+	class Track:
+		var beat_map: Array[int]
+		var beat_index: int = 0
+		var future_beat_offset: int
+		var future_beat_sent: bool = false
+
+		func get_beat():
+			return [beat_map[beat_index], beat_map[beat_index + 1]]
+
+		func next_beat():
+			beat_index += 1
+			future_beat_sent = false
+			return get_beat()
+			
+		func reset():
+			beat_index = 0
+			future_beat_sent = false
+
 func _ready():
 	add_child(audio)
 	delay = AudioServer.get_time_to_next_mix() + AudioServer.get_output_latency()
 	
-	var chars_dir = DirAccess.open("res://music/beatmaps")
-	chars_dir.list_dir_begin()
-	var folder = chars_dir.get_next()
+	var music_dir = DirAccess.open("res://music/beatmaps")
+	music_dir.list_dir_begin()
+	var folder = music_dir.get_next()
 	
 	while folder != "":
 		var maps = {}
-		var char_dir = DirAccess.open("res://music/beatmaps" + folder)
-		char_dir.list_dir_begin()
-		var asset = char_dir.get_next()
+		var beat_dir = DirAccess.open("res://music/beatmaps/" + folder)
+		beat_dir.list_dir_begin()
+		var asset = beat_dir.get_next()
 		
 		while asset != "" && asset.ends_with(".beat"):
 			var all_bytes: PackedByteArray = FileAccess.get_file_as_bytes("res://music/beatmaps/" + folder + "/" + asset)
-			var bytes: PackedByteArray
-			var track_offset = 0
+			var track_offset = 1
 			
 			# Loop through all the tracks
 			for i in range(all_bytes.decode_u8(0)):
-				var end = bytes.find(0x0a)
-				bytes = all_bytes.slice(track_offset, end - 1)
+				var end = all_bytes.find(0x0b)
+				var bytes = all_bytes.slice(track_offset, end)
 				track_offset += end
+				
 				# Track name
-				var offset = bytes.find(0x0b)
-				#var track_name = bytes.slice(0, offset - 1).get_string_from_utf8()
+				var offset = bytes.find(0x0a)
+				var track_name = bytes.slice(0, offset).get_string_from_utf8()
 				offset += 1
+				
 				# Track data
-				var beat_map: Array = []
-				while end > offset:
-					beat_map.append(bytes.decode_float(offset))
+				var beat_map = []
+				while end - 1 > offset:
+					beat_map.append(bytes.decode_u32(offset))
 					offset += 4
-				maps[name] = beat_map
-			asset = char_dir.get_next()
+				maps[track_name] = beat_map
+			asset = beat_dir.get_next()
 		
 		beat_maps[folder] = maps
-		folder = chars_dir.get_next()
-		
+		folder = music_dir.get_next()
+	
 	audio.play()
 
 func _process(_delta):
@@ -79,27 +104,3 @@ func start_session(player_id: int, beat_map):
 
 func end_session(player_id: int):
 	sessions[player_id] = null
-
-class Session:
-	var tracks: Dictionary
-	
-	func _init(map: String):
-		tracks = RhythmEngine.beat_maps[map]
-	
-	class Track:
-		var beat_map: Array[float]
-		var beat_index: int = 0
-		var future_beat_offset: int
-		var future_beat_sent: bool = false
-
-		func get_beat():
-			return [beat_map[beat_index], beat_map[beat_index + 1]]
-
-		func next_beat():
-			beat_index += 1
-			future_beat_sent = false
-			return get_beat()
-			
-		func reset():
-			beat_index = 0
-			future_beat_sent = false
