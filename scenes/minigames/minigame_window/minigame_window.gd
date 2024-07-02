7579de0e658d43bdeb845cc7fd57aff190726648
Ethan Meier <e652@hotmail.ca>
2024-07-01 23:52:23 -0700

    cleanup window manager testing

--- scenes/minigames/minigame_window/minigame_window.gd
+++ scenes/minigames/minigame_window/minigame_window.gd
@@ -3,28 +3,26 @@ extends NinePatchRect
 
 var progress_bar: ProgressBar
 
-func setup_direction(width: int, height: int):
+func setup_direction(window_size: Vector2):
 	progress_bar = find_child(&"ProgressBar")
 
-	if width > height: # horizontal
-		print("horizontal")
+	if window_size.x > window_size.y: # horizontal
 		region_rect = Rect2(2, 0, 3, 5)
 		patch_margin_top = 3
 		progress_bar.position = Vector2(1, 1)
-		progress_bar.size = Vector2(width - 2, 1)
+		progress_bar.size = Vector2(window_size.x - 2, 1)
 		progress_bar.fill_mode = ProgressBar.FILL_BEGIN_TO_END
 	else: # vertical
-		print("vertical")
 		region_rect = Rect2(0, 0, 5, 3)
 		patch_margin_left = 3
 		progress_bar.position = Vector2(1, 1)
-		progress_bar.size = Vector2(1, height - 2)
+		progress_bar.size = Vector2(1, window_size.y - 2)
 		progress_bar.fill_mode = ProgressBar.FILL_BOTTOM_TO_TOP
 	progress_bar.value = 15
 
-func place_window(window_position: Vector2, width: int, height: int, _minigame_scene: PackedScene):
-	size = Vector2(width, height)
-	position = Vector2(window_position.x - width / 2.0, window_position.y - height / 2.0)
+func place_window(window_position: Vector2, window_size: Vector2, _minigame_scene: PackedScene):
+	size = Vector2(window_size.x, window_size.y)
+	position = Vector2(window_position.x - int(window_size.x / 2), window_position.y - int(window_size.y / 2))
 	# var minigame = minigame_scene.instantiate() as Node2D
 	# add_child(minigame)
-	setup_direction(width, height)
+	setup_direction(window_size)
--- scenes/minigames/minigame_window/window_manager.gd
+++ scenes/minigames/minigame_window/window_manager.gd
@@ -8,7 +8,6 @@ enum Minigames {
 }
 
 var window_scene = load("res://scenes/minigames/minigame_window/minigame_window.tscn") as PackedScene
-var windows: Array[StaticBody2D] = []
 
 func load_minigame(minigame: Minigames) -> PackedScene:
 	match minigame:
@@ -18,11 +17,25 @@ func load_minigame(minigame: Minigames) -> PackedScene:
 			push_error("No minigame scene provided for " + Minigames.keys()[minigame])
 			return load("")
 
-func calculate_position(initial_position: Vector2) -> Vector2:
+# Does this really need to be hardcoded?
+func get_window_size(minigame: Minigames) -> Vector2:
+	match minigame:
+		Minigames.Fishing:
+			return Vector2(30, 55)
+		_:
+			push_error("No minigame size provided for " + Minigames.keys()[minigame])
+			return Vector2.ZERO
+
+# TODO: Implement this
+func calculate_position(initial_position: Vector2, window_size: Vector2) -> Vector2:
 	return Vector2.ZERO
+	for window: Control in get_children():
+		# determine if the window has a possible position
+		pass
 
 func create_window(initial_position: Vector2, minigame: Minigames):
 	var minigame_window = window_scene.instantiate() as MinigameWindow
 	add_child(minigame_window)
-	var window_position = calculate_position(initial_position)
-	minigame_window.place_window(window_position, 40, 70, load_minigame(minigame))
+	var window_size: Vector2 = get_window_size(minigame)
+	var window_position: Vector2 = calculate_position(initial_position, window_size)
+	minigame_window.place_window(window_position, window_size, load_minigame(minigame))
