6d18b43cee1e5a14e1be544d054cd1621ec1cfb7
Ethan Meier <e652@hotmail.ca>
2024-07-03 01:24:54 -0700

    got further with the window reservation and location system

--- scenes/minigames/minigame_window/minigame_window.gd
+++ scenes/minigames/minigame_window/minigame_window.gd
@@ -3,26 +3,26 @@ extends NinePatchRect
 
 var progress_bar: ProgressBar
 
-func setup_direction(window_size: Vector2):
+func setup_direction(window_size: Vector2i):
 	progress_bar = find_child(&"ProgressBar")
 
 	if window_size.x > window_size.y: # horizontal
 		region_rect = Rect2(2, 0, 3, 5)
 		patch_margin_top = 3
-		progress_bar.position = Vector2(1, 1)
-		progress_bar.size = Vector2(window_size.x - 2, 1)
+		progress_bar.position = Vector2i(1, 1)
+		progress_bar.size = Vector2i(window_size.x - 2, 1)
 		progress_bar.fill_mode = ProgressBar.FILL_BEGIN_TO_END
 	else: # vertical
 		region_rect = Rect2(0, 0, 5, 3)
 		patch_margin_left = 3
-		progress_bar.position = Vector2(1, 1)
-		progress_bar.size = Vector2(1, window_size.y - 2)
+		progress_bar.position = Vector2i(1, 1)
+		progress_bar.size = Vector2i(1, window_size.y - 2)
 		progress_bar.fill_mode = ProgressBar.FILL_BOTTOM_TO_TOP
 	progress_bar.value = 15
 
-func place_window(window_position: Vector2, window_size: Vector2, _minigame_scene: PackedScene):
+func place_window(window_position: Vector2i, window_size: Vector2i, _minigame_scene: PackedScene):
 	size = Vector2(window_size.x, window_size.y)
-	position = Vector2(window_position.x - int(window_size.x / 2), window_position.y - int(window_size.y / 2))
+	position = Vector2i(window_position.x - window_size.x / 2, window_position.y - window_size.y / 2)
 	# var minigame = minigame_scene.instantiate() as Node2D
 	# add_child(minigame)
 	setup_direction(window_size)
--- scenes/minigames/minigame_window/window_manager.gd
+++ scenes/minigames/minigame_window/window_manager.gd
@@ -8,6 +8,46 @@ enum Minigames {
 }
 
 var window_scene = load("res://scenes/minigames/minigame_window/minigame_window.tscn") as PackedScene
+var pixel_mapping: Array[Array] # 2d grid of booleans, representing the available space for windows. true is occupied
+
+@onready var viewport_height: int = ProjectSettings.get_setting("display/window/size/viewport_height")
+@onready var viewport_width: int = ProjectSettings.get_setting("display/window/size/viewport_width")
+
+# === Pixel Grid Management
+
+func _ready():
+	# populate pixel grid
+	for x in viewport_width:
+		pixel_mapping.append([])
+		for y in viewport_height:
+			pixel_mapping[x].append(false)
+
+# This function does *not* do any verification, handle that yourself nerd
+func reserve_window(window_size: Vector2i, window_position: Vector2i):
+	for x in range(window_position.x, window_position.x + window_size.x):
+		for y in range(window_position.y, window_position.y + window_size.y):
+			pixel_mapping[x][y] = true
+
+func free_window(window_size: Vector2i, window_position: Vector2i):
+	for x in range(window_position.x, window_position.x + window_size.x):
+		for y in range(window_position.y, window_position.y + window_size.y):
+			pixel_mapping[x][y] = false
+
+# returns the first positional int closest to 0 (without crossing 0) that fits the size provided
+# or return null if no size is found
+func find_nearest_space(window_position: Vector2i, window_size: Vector2i): # -> int:
+	# organize neighbours onto heap (or queue?) prioritised by distance (nearest first) (dx^2 + dy^2 or dx + dy)
+	# pop each item off the ADT, if it's free use it, if not add it's neighbours to the ADT
+	# https://stackoverflow.com/questions/17734591/algorithm-finding-closest-empty-square-a-2d-grid#comment25854085_17734711
+	return 0
+
+# TODO: Implement this
+func calculate_position(initial_position: Vector2i, window_size: Vector2i) -> Vector2i:
+	return initial_position
+
+	# determine if there is enough vertical room to fit
+
+# === Window Management
 
 func load_minigame(minigame: Minigames) -> PackedScene:
 	match minigame:
@@ -21,21 +61,16 @@ func load_minigame(minigame: Minigames) -> PackedScene:
 func get_window_size(minigame: Minigames) -> Vector2:
 	match minigame:
 		Minigames.Fishing:
-			return Vector2(30, 55)
+			return Vector2i(30, 55)
+		Minigames.Cutting:
+			return Vector2i(55, 30)
 		_:
 			push_error("No minigame size provided for " + Minigames.keys()[minigame])
 			return Vector2.ZERO
 
-# TODO: Implement this
-func calculate_position(initial_position: Vector2, window_size: Vector2) -> Vector2:
-	return Vector2.ZERO
-	for window: Control in get_children():
-		# determine if the window has a possible position
-		pass
-
-func create_window(initial_position: Vector2, minigame: Minigames):
+func create_window(initial_position: Vector2i, minigame: Minigames):
 	var minigame_window = window_scene.instantiate() as MinigameWindow
 	add_child(minigame_window)
-	var window_size: Vector2 = get_window_size(minigame)
-	var window_position: Vector2 = calculate_position(initial_position, window_size)
+	var window_size: Vector2i = get_window_size(minigame)
+	var window_position: Vector2i = calculate_position(initial_position, window_size)
 	minigame_window.place_window(window_position, window_size, load_minigame(minigame))
